375e8fefffbca485c99614a29ff65f52abbb50a9
Hanas <hanasoke@gmail.com>
2025-11-27 09:33:42 +0700

    updated motorcyle_controller file

--- controllers/motorcycle_controller.py
+++ controllers/motorcycle_controller.py
@@ -15,3 +15,23 @@ class MotorcycleController:
         motorcycle = Motorcycle(code, brand, type, stock, price)
         self.model.add(motorcycle)
         self.view.show_message("The Motorcycle has been added!")
+        
+    def show_motorcycle(self): 
+        code = input("The Motorcycle Code has been deleted: ")
+        m = self.model.search(code)
+        if m: 
+            self.model.delete(m)
+            self.view.show_message("deleted successfully!")
+    
+    def show_motorcycle(self): 
+        data = self.model.all()
+        self.view.show_all(data)
+        
+    def delete_motorcycle(self): 
+        code = input("The Motor Code want to be deleted: ")
+        m = self.model.search(code)
+        if m: 
+            self.model.delete(m)
+            self.view.show_message("Deleted Successfully!")
+        else:
+            self.view.show_message("No Found This Motorcycle!")
