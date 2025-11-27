c711d1fc5ac58d4307da20b08539b2380b693e99
Hanas <hanasoke@gmail.com>
2025-11-27 08:27:22 +0700

    updated files in models folder

---
+++ models/motor.py
@@ -0,0 +1,7 @@
+class Motor: 
+    def __init__(self, code, brand, type, stock, price):
+        self.code = code 
+        self.brand = brand 
+        self.type = type 
+        self.stock = stock
+        self.price = price  
---
+++ models/motor_model.py
@@ -0,0 +1,19 @@
+class MotorModel: 
+    def __init__(self):
+        self.data = []
+        
+    def add(self, motorcycle):
+        self.data.append(motorcycle)
+        
+    def all(self):
+        return self.data 
+    
+    def search(self, code):
+        for m in self.data: 
+            if m.code == code: 
+                return m 
+            return None 
+        return None 
+    
+    def delete(self, motorcycle):
+        self.data.remove(motorcycle)
